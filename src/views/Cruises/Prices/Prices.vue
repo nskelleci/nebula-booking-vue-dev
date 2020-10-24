f24d301ccd99b4df36a22d57848060e9596ffcd0
Nazmi Sefa Kelleci <nskelleci@gmail.com>
2020-10-25 02:08:46 +0300

    prices component design fixed

--- src/views/Cruises/Prices/Prices.vue
+++ src/views/Cruises/Prices/Prices.vue
@@ -8,145 +8,179 @@
 ========================================================================================== -->
 
 <template>
-<vs-row>
-     <vs-col vs-type="flex" vs-justify="center" vs-align="center" vs-w="4">
-         <vs-table ref="table"   v-model="selected" :data="vessels">
+<div class="grid-demo__layout-container">
+  <vs-row vs-w="12">
+    <vs-col vs-type="flex"   vs-lg="4" vs-sm="4" vs-xs="12"  >
+      <vs-table ref="vesselTable" v-model="selected" :data="vessels">
+        <template slot="thead">
+          <vs-th sort-key="name">Select Vessel</vs-th>
+        </template>
 
-      <template slot="thead">
-        <vs-th sort-key="name">Name</vs-th>    
-      </template>
-
-      <template slot-scope="{data}">
-        <tbody>
-          <vs-tr :data="vessel" :key="_id" v-for="(vessel,_id) in data" >
-            <vs-td>
-              <p class="product-name font-medium truncate">{{ vessel.name }}</p>
-            </vs-td>
-          </vs-tr>
-        </tbody>
-      </template>
-    </vs-table>
+        <template slot-scope="{ data }">
+          <tbody>
+            <vs-tr :data="vessel" :key="_id" v-for="(vessel, _id) in data">
+              <vs-td>
+                <p class="product-name font-medium truncate">
+                  {{ vessel.name }}
+                </p>
+              </vs-td>
+            </vs-tr>
+          </tbody>
+        </template>
+      </vs-table>
     </vs-col>
+    <vs-col vs-type="block"  vs-align="center" vs-lg="8" vs-sm="4" vs-xs="12"> 
+      <div
+        id="data-list-thumb-view"
+        class="data-list-container"
+      >
+        <price-form
+          :isSidebarActive="addNewDataSidebar"
+          @closeSidebar="toggleDataSidebar"
+          :data="sidebarData"
+        />
 
+        <vs-table
+          ref="priceTable"
+          v-model="selected"
+          pagination
+          :max-items="itemsPerPage"
+          search
+          :data="ports"
+        >
+          <div
+            slot="header"
+            class="flex flex-wrap-reverse items-center flex-grow justify-between"
+          >
+            <div class="flex flex-wrap-reverse items-center">
+              <!-- ACTION - DROPDOWN -->
+              <vs-dropdown vs-trigger-click class="cursor-pointer mr-4 mb-4">
+                <div
+                  class="p-4 shadow-drop rounded-lg d-theme-dark-bg cursor-pointer flex items-center justify-center text-lg font-medium w-32"
+                >
+                  <span class="mr-2">Actions</span>
+                  <feather-icon icon="ChevronDownIcon" svgClasses="h-4 w-4" />
+                </div>
 
+                <vs-dropdown-menu>
+                  <vs-dropdown-item>
+                    <span class="flex items-center">
+                      <feather-icon
+                        icon="TrashIcon"
+                        svgClasses="h-4 w-4"
+                        class="mr-2"
+                      />
+                      <span>Delete</span>
+                    </span>
+                  </vs-dropdown-item>
+                </vs-dropdown-menu>
+              </vs-dropdown>
 
-
-    <vs-col vs-type="flex" vs-justify="center" vs-align="center" vs-w="8">
-    <div id="data-list-thumb-view  vx-col lg:w-2/3 w-full relative" class="data-list-container">
-
-    <price-form :isSidebarActive="addNewDataSidebar" @closeSidebar="toggleDataSidebar" :data="sidebarData" />
-
-    <vs-table ref="table"   v-model="selected" pagination :max-items="itemsPerPage" search :data="ports">
-
-      <div slot="header" class="flex flex-wrap-reverse items-center flex-grow justify-between">
-
-        <div class="flex flex-wrap-reverse items-center">
-
-          <!-- ACTION - DROPDOWN -->
-          <vs-dropdown vs-trigger-click class="cursor-pointer mr-4 mb-4">
-
-            <div class="p-4 shadow-drop rounded-lg d-theme-dark-bg cursor-pointer flex items-center justify-center text-lg font-medium w-32">
-              <span class="mr-2">Actions</span>
-              <feather-icon icon="ChevronDownIcon" svgClasses="h-4 w-4" />
+              <!-- ADD NEW -->
+              <div
+                class="p-3 mb-4 mr-4 rounded-lg cursor-pointer flex items-center justify-between text-lg font-medium text-base text-primary border border-solid border-primary"
+                @click="addNewData"
+              >
+                <feather-icon icon="PlusIcon" svgClasses="h-4 w-4" />
+                <span class="ml-2 text-base text-primary">Add New</span>
+              </div>
             </div>
 
-            <vs-dropdown-menu>
-
-              <vs-dropdown-item>
-                <span class="flex items-center">
-                  <feather-icon icon="TrashIcon" svgClasses="h-4 w-4" class="mr-2" />
-                  <span>Delete</span>
-                </span>
-              </vs-dropdown-item>
-            </vs-dropdown-menu>
-          </vs-dropdown>
-
-          <!-- ADD NEW -->
-          <div class="p-3 mb-4 mr-4 rounded-lg cursor-pointer flex items-center justify-between text-lg font-medium text-base text-primary border border-solid border-primary" @click="addNewData">
-              <feather-icon icon="PlusIcon" svgClasses="h-4 w-4" />
-              <span class="ml-2 text-base text-primary">Add New</span>
+            <!-- ITEMS PER PAGE -->
+            <vs-dropdown vs-trigger-click class="cursor-pointer mb-4 mr-4">
+              <div
+                class="p-4 border border-solid d-theme-border-grey-light rounded-full d-theme-dark-bg cursor-pointer flex items-center justify-between font-medium"
+              >
+                <span class="mr-2"
+                  >{{ currentPage * itemsPerPage - (itemsPerPage - 1) }} -
+                  {{
+                    ports.length - currentPage * itemsPerPage > 0
+                      ? currentPage * itemsPerPage
+                      : ports.length
+                  }}
+                  of {{ queriedItems }}</span
+                >
+                <feather-icon icon="ChevronDownIcon" svgClasses="h-4 w-4" />
+              </div>
+              <!-- <vs-button class="btn-drop" type="line" color="primary" icon-pack="feather" icon="icon-chevron-down"></vs-button> -->
+              <vs-dropdown-menu>
+                <vs-dropdown-item @click="itemsPerPage = 4">
+                  <span>4</span>
+                </vs-dropdown-item>
+                <vs-dropdown-item @click="itemsPerPage = 10">
+                  <span>10</span>
+                </vs-dropdown-item>
+                <vs-dropdown-item @click="itemsPerPage = 15">
+                  <span>15</span>
+                </vs-dropdown-item>
+                <vs-dropdown-item @click="itemsPerPage = 20">
+                  <span>20</span>
+                </vs-dropdown-item>
+              </vs-dropdown-menu>
+            </vs-dropdown>
           </div>
-        </div>
 
+          <template slot="thead">
+            <vs-th>Image</vs-th>
+            <vs-th sort-key="name">Name</vs-th>
+            <vs-th sort-key="category">Flag</vs-th>
+            <vs-th>Action</vs-th>
+          </template>
 
-        <!-- ITEMS PER PAGE -->
-        <vs-dropdown vs-trigger-click class="cursor-pointer mb-4 mr-4">
-          <div class="p-4 border border-solid d-theme-border-grey-light rounded-full d-theme-dark-bg cursor-pointer flex items-center justify-between font-medium">
-            <span class="mr-2">{{ currentPage * itemsPerPage - (itemsPerPage - 1) }} - {{ ports.length - currentPage * itemsPerPage > 0 ? currentPage * itemsPerPage : ports.length }} of {{ queriedItems }}</span>
-            <feather-icon icon="ChevronDownIcon" svgClasses="h-4 w-4" />
-          </div>
-          <!-- <vs-button class="btn-drop" type="line" color="primary" icon-pack="feather" icon="icon-chevron-down"></vs-button> -->
-          <vs-dropdown-menu>
+          <template slot-scope="{ data }">
+            <tbody>
+              <vs-tr :data="port" :key="_id" v-for="(port, _id) in data">
+                <vs-td class="img-container">
+                  <!-- <img :src="tr.img" class="product-img" /> -->
+                </vs-td>
+                <vs-td>
+                  <p class="product-name font-medium truncate">
+                    {{ port.country.name }}
+                  </p>
+                </vs-td>
+                <vs-td>
+                  <p class="product-name font-medium truncate">
+                    {{ port.name }}
+                  </p>
+                </vs-td>
 
-            <vs-dropdown-item @click="itemsPerPage=4">
-              <span>4</span>
-            </vs-dropdown-item>
-            <vs-dropdown-item @click="itemsPerPage=10">
-              <span>10</span>
-            </vs-dropdown-item>
-            <vs-dropdown-item @click="itemsPerPage=15">
-              <span>15</span>
-            </vs-dropdown-item>
-            <vs-dropdown-item @click="itemsPerPage=20">
-              <span>20</span>
-            </vs-dropdown-item>
-          </vs-dropdown-menu>
-        </vs-dropdown>
+                <vs-td class="whitespace-no-wrap">
+                  <!-- <feather-icon icon="InfoIcon" svgClasses="w-7 h-7 hover:text-primary stroke-current" @click="portDetail(port)" /> -->
+                  <feather-icon
+                    icon="EditIcon"
+                    svgClasses="w-7 h-7 hover:text-primary stroke-current"
+                    class="ml-4"
+                    @click.stop="editData(port)"
+                  />
+                  <feather-icon
+                    icon="TrashIcon"
+                    svgClasses="w-7 h-7 hover:text-danger stroke-current"
+                    class="ml-4"
+                    @click.stop="deleteData(port._id)"
+                  />
+                </vs-td>
+              </vs-tr>
+            </tbody>
+          </template>
+        </vs-table>
       </div>
-
-      <template slot="thead">
-        <vs-th>Image</vs-th>
-        <vs-th sort-key="name">Name</vs-th>
-        <vs-th sort-key="category">Flag</vs-th>        
-        <vs-th>Action</vs-th>
-      </template>
-
-      <template slot-scope="{data}">
-        <tbody>
-          <vs-tr :data="port" :key="_id" v-for="(port,_id) in data" >
-
-            <vs-td class="img-container">
-              <!-- <img :src="tr.img" class="product-img" /> -->
-            </vs-td>
-            <vs-td>
-              <p class="product-name font-medium truncate">{{ port.country.name }}</p>
-            </vs-td>
-            <vs-td>
-              <p class="product-name font-medium truncate">{{ port.name }}</p>
-            </vs-td>
-
-            <vs-td class="whitespace-no-wrap">
-              <!-- <feather-icon icon="InfoIcon" svgClasses="w-7 h-7 hover:text-primary stroke-current" @click="portDetail(port)" /> -->
-              <feather-icon icon="EditIcon" svgClasses="w-7 h-7 hover:text-primary stroke-current" class="ml-4" @click.stop="editData(port)" />
-              <feather-icon icon="TrashIcon" svgClasses="w-7 h-7 hover:text-danger stroke-current" class="ml-4" @click.stop="deleteData(port._id)" />
-            </vs-td>
-          </vs-tr>
-        </tbody>
-      </template>
-    </vs-table>
-  </div>
     </vs-col>
-   
+  </vs-row>
 
-
-
-</vs-row>
-  
+</div>
   
 </template>
 
 <script>
-
-import PriceForm  from './PriceForm'
-import Vessels  from '../../Vessel/vessels'
+import PriceForm from './PriceForm'
 import router from '../../../router'
 //import moduleDataList from '@/store/data-list/moduleDataList.js'
 
 export default {
   components: {
-    PriceForm,
-    Vessels
+    PriceForm
   },
+
   data () {
     return {
       selected: [],
@@ -160,7 +194,9 @@ export default {
   computed: {
     currentPage () {
       if (this.isMounted) {
-        return this.$refs.table.currentx
+        return this.$refs.vesselTable.currentx
+        return this.$refs.priceTable.currentx
+
       }
       return 0
     },
@@ -170,7 +206,7 @@ export default {
     // },
     queriedItems () {
       return []
-    // return this.$refs.table ? this.$refs.table.queriedResults.length : this.products.length
+      // return this.$refs.table ? this.$refs.table.queriedResults.length : this.products.length
     },
     ports () {
       return this.$store.state.port.ports
@@ -192,25 +228,26 @@ export default {
     toggleDataSidebar (val = false) {
       this.addNewDataSidebar = val
     },
-    // async fetchVessels () {      
+    // async fetchVessels () {
     //   this.vessels = await VesselService.fetchVessels()
     // },
-    
+
     portDetail (port) {
-      router.push({name : 'port-detail', params: {id: port._id} })
+      router.push({ name: 'port-detail', params: { id: port._id } })
     }
   },
   created () {
-    this.$store.dispatch('getPorts'),
+    this.$store.dispatch('getPorts') 
     this.$store.dispatch('getVessels')
   },
   updated () {
-   
+
   },
   mounted () {
     this.isMounted = true
   },
-  watch :  {         
+  watch: {
+
   }
 }
 </script>
@@ -218,7 +255,6 @@ export default {
 <style lang="scss">
 #data-list-thumb-view {
   .vs-con-table {
-
     .product-name {
       max-width: 23rem;
     }
@@ -233,18 +269,18 @@ export default {
         flex-grow: 1;
       }
 
-      .vs-table--search{
+      .vs-table--search {
         padding-top: 0;
 
         .vs-table--search-input {
           padding: 0.9rem 2.5rem;
           font-size: 1rem;
 
-          &+i {
+          & + i {
             left: 1rem;
           }
 
-          &:focus+i {
+          &:focus + i {
             left: 1rem;
           }
         }
@@ -256,53 +292,52 @@ export default {
       border-spacing: 0 1.3rem;
       padding: 0 1rem;
 
+      tr {
+        box-shadow: 0 4px 20px 0 rgba(0, 0, 0, 0.05);
+        td {
+          padding: 10px;
+          &:first-child {
+            border-top-left-radius: 0.5rem;
+            border-bottom-left-radius: 0.5rem;
+          }
+          &:last-child {
+            border-top-right-radius: 0.5rem;
+            border-bottom-right-radius: 0.5rem;
+          }
+          &.img-container {
+            // width: 1rem;
+            // background: #fff;
 
-      tr{
-          box-shadow: 0 4px 20px 0 rgba(0,0,0,.05);
-          td{
-            padding: 10px;
-            &:first-child{
-              border-top-left-radius: .5rem;
-              border-bottom-left-radius: .5rem;
+            span {
+              display: flex;
+              justify-content: flex-start;
             }
-            &:last-child{
-              border-top-right-radius: .5rem;
-              border-bottom-right-radius: .5rem;
-            }
-            &.img-container {
-              // width: 1rem;
-              // background: #fff;
 
-              span {
-                display: flex;
-                justify-content: flex-start;
-              }
-
-              .product-img {
-                height: 110px;
-              }
+            .product-img {
+              height: 110px;
             }
           }
-          td.td-check{
-            padding: 20px !important;
-          }
+        }
+        td.td-check {
+          padding: 20px !important;
+        }
       }
     }
 
-    .vs-table--thead{
+    .vs-table--thead {
       th {
         padding-top: 0;
         padding-bottom: 0;
 
-        .vs-table-text{
+        .vs-table-text {
           text-transform: uppercase;
           font-weight: 600;
         }
       }
-      th.td-check{
+      th.td-check {
         padding: 0 15px !important;
       }
-      tr{
+      tr {
         background: none;
         box-shadow: none;
       }
